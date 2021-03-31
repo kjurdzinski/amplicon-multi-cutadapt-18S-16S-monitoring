2c3242f59b7cfe8fa8860142630f81ea8f22aa7c
johnne <john.sundh@scilifelab.se>
2021-04-01 00:04:28 +0200

    Clean up code

--- workflow/scripts/common.py
+++ workflow/scripts/common.py
@@ -5,26 +5,6 @@ from tqdm import tqdm
 import sys
 
 
-def extract_species_fasta(seq_df, outdir, species):
-    species_map = {}
-    ranks = ["phylum", "class", "order", "family", "genus", "species"]
-    i = -1
-    for sp in tqdm(species, desc=f"Writing species fasta to {outdir}",
-                   unit=" species"):
-        i += 1
-        species_map[i] = sp
-        records = seq_df.loc[seq_df.species == sp]
-        records = records.fillna("")
-        with open(f"{outdir}/{i}.fasta", 'w') as fhout:
-            for index in records.index:
-                seq = records.loc[index, "seq"]
-                record_id = records.loc[index, "record_id"]
-                seq = seq.replace("-", "").strip("N")
-                desc = ";".join([records.loc[index, x] for x in ranks])
-                fhout.write(f">{record_id} {desc}\n{seq}\n")
-    return species_map
-
-
 def write_seqs(seq_df, outfile):
     ranks = ["phylum", "class", "order", "family", "genus", "species"]
     seq_df = seq_df.sort_values("species")
@@ -47,7 +27,9 @@ def filter_bold(sm):
     sys.stderr.write(f"Reading info file {sm.input[0]}\n")
     df = pd.read_csv(sm.input[0], sep="\t", usecols=[0, 4, 7, 8, 9, 10, 11, 12],
                      names=["record_id", "bold_id", "phylum", "class", "order",
-                            "family", "genus", "species"])
+                            "family", "genus", "species"],
+                     dtype={'bold_id': str})
+    df.fillna("", inplace=True)
     sys.stderr.write(f"{df.shape[0]} records read\n")
     # Filter dataframe to phyla
     sys.stderr.write("Filtering info file\n")
@@ -72,8 +54,6 @@ def filter_bold(sm):
                                                index=False, sep="\t")
     # Write seqs to file
     write_seqs(seq_df, sm.output.fasta)
-
-    # # Extract species fasta  # species = seq_df.species.unique()  # species_map = extract_species_fasta(seq_df, sm.output.sp_dir, species)  # species_map_df = pd.DataFrame(species_map, index=[0]).T  # species_map_df.index.name = "species_id"  # species_map_df.columns = ["species"]  # species_map_df.to_csv(sm.output.sp_map)
 
 
 def main(sm):
